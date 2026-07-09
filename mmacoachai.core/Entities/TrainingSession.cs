f4d4df77b839a328320d2691da5a9bb88f77a298
Esai <esaibarron@gmail.com>
2026-07-08 22:24:35 -0500

    Configured Entity Freamwork Core and DB Context

---
+++ mmacoachai.core/Entities/TrainingSession.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mmacoachai.core.Entities
+{
+    public class TrainingSession
+    {
+        public int id { get; set; }
+        public int AthleteId { get; set; }
+        public DateTime Date { get; set; }
+        public int DurationMinutes { get; set; }
+        public String Notes { get; set; } = string.Empty;
+
+        public Athlete Athlete { get; set; } = null;
+    }
+}
